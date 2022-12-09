468dbae27b03e5e665178b69df5d7ff1666fbd0a
João Eliandro <eliandrogermano@gmail.com>
2022-12-09 01:01:13 -0300

    chore: corrigido path do output da estimativa

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ st.table(matriz.applymap(aux))
 st.markdown('---')
 st.markdown("## 🌍 Probabilidades dos Jogos da Copa") 
 
-jogoscopa = pd.read_excel('dados/outputEstimativasJogosCopa.xlsx', index_col = 0)
+jogoscopa = pd.read_excel('data/outputEstimativaJogosCopa2022.xlsx', index_col = 0)
 st.table(jogoscopa[['grupo', 'seleção1', 'seleção2', 'Vitória', 'Empate', 'Derrota']])
 
 
